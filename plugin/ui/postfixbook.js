1f9ab1ad36c9ee8b2179fb0e859bedf778de2bef
Leonid Orsulic <leonid.orsulic@nuvariant.com>
2022-06-02 15:58:02 +0200

    add main postfixbook js plugin file

---
+++ plugin/ui/postfixbook.js
@@ -0,0 +1,12 @@
+define([ 
+    './mailalias', 
+    './mailenabled', 
+    './mailforwardingaddress', 
+    './mailgidnumber', 
+    './mailgroupmember', 
+    './mailhomedirectory', 
+    './mailquota', 
+    './mailsieverulesource', 
+    './mailstoragedirectory', 
+    './mailuidnumber'],
+    function(){ });
