20e95068be2ec10bb59e5b54665b4b466b884a01
Leonid Orsulic <leonid.orsulic@nuvariant.com>
2022-06-04 17:26:06 +0200

    schema and add_missing_object_class
    - clean up schema
    - refactor python files to use add_missing_object_class

--- plugin/ipaserver/plugins/mailhomedirectory.py
+++ plugin/ipaserver/plugins/mailhomedirectory.py
@@ -1,6 +1,7 @@
 from ipaserver.plugins import user
 from ipalib.parameters import Str
 from ipalib.text import _
+from .baseldap import add_missing_object_class
 
 user.user.takes_params = user.user.takes_params + (
     Str(
@@ -20,7 +21,8 @@ user.user.default_attributes.append("mailhomedirectory")
 # pylint: disable-msg=unused-argument,invalid-name,line-too-long
 def useradd_precallback(self, ldap, dn, entry, attrs_list, *keys, **options):
 
-    entry["objectclass"].append("postfixbookmailaccount")
+    #entry["objectclass"].append("postfixbookmailaccount")
+    add_missing_object_class(ldap, 'postfixbookmailaccount', dn)
     return dn
 
 
@@ -30,10 +32,11 @@ user.user_add.register_pre_callback(useradd_precallback)
 # pylint: disable-msg=unused-argument,invalid-name,line-too-long
 def usermod_precallback(self, ldap, dn, entry, attrs_list, *keys, **options):
 
-    if "objectclass" not in entry.keys():
-        old_entry = ldap.get_entry(dn, ["objectclass"])
-        entry["objectclass"] = old_entry["objectclass"]
-    entry["objectclass"].append("postfixbookmailaccount")
+    #if "objectclass" not in entry.keys():
+    #    old_entry = ldap.get_entry(dn, ["objectclass"])
+    #    entry["objectclass"] = old_entry["objectclass"]
+    #entry["objectclass"].append("postfixbookmailaccount")
+    add_missing_object_class(ldap, 'postfixbookmailaccount', dn)
     return dn
 
 
